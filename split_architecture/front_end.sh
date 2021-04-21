c670995f3d88e615952a77214736c3ac8ebf2415
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-04-21 13:14:24 -0700

    Update front_end.sh

--- split_architecture/front_end.sh
+++ split_architecture/front_end.sh
@@ -15,4 +15,6 @@ git clone https://github.com/nic-instruction/stuff.git
 cp stuff/app/* /var/www/html/
 
 # Then cd to /var/www/html/ and edit the php files to have your db address and connection info.
+# Automate this part using git or wget and sed for the password and db search and replace on the php files.  
+# remember that the git clone dir will be in /root.
 
