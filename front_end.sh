59c476628a489ec714d84bc9a06a9f90c4cab890
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-04-20 17:24:10 -0700

    Update front_end.sh

--- front_end.sh
+++ front_end.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum -y install httpd mod_php php-mysql mod_ssl
+yum -y install httpd mod_php php-mysql mod_ssl unzip
 
 echo "<?php
 
@@ -9,3 +9,10 @@ phpinfo ();
 ?>" > /var/www/html/info.php
 
 systemctl start httpd
+
+# upload files
+# mv /home/nicolebade/helloworld\ \(1\).zip /var/www/html (replace nicolebade with your username)
+# Now you can go to http://34.68.83.204/helloworld/index.php  (replace 34.68.83.204 with your ip) and see a page
+
+# edit everything to connect to your database server!  (connection is in functions)
+
