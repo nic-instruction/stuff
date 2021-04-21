3276783236f31ba137e6ba4c91912fb178997d61
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-04-20 18:27:13 -0700

    Update back_end.sql

--- back_end.sql
+++ back_end.sql
@@ -8,5 +8,12 @@ CREATE TABLE info (
     sarcastic_gloat varchar(1000)
 );
 
+CREATE USER 'MyApp'@'%' IDENTIFIED BY PASSWORD '*236023AE71553ECD5E234575E2EEC41C32263119';
 GRANT USAGE ON *.* TO 'MyApp'@'localhost' IDENTIFIED BY PASSWORD '*236023AE71553ECD5E234575E2EEC41C32263119';
 GRANT SELECT, INSERT, UPDATE ON `back\_end`.* TO 'MyApp'@'localhost';
+
+# Enable remote connections
+# open /etc/my.cnf
+# put in 
+# bind-address = 0.0.0.0
+# right below # instructions in http://fedoraproject.org/wiki/Systemd
