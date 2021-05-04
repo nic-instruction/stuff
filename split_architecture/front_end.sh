0d2b56ff20e891e9fe2fd1ed57eeaa0ee989fcdd
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-04 16:44:25 -0700

    Update front_end.sh

--- split_architecture/front_end.sh
+++ split_architecture/front_end.sh
@@ -11,6 +11,10 @@ phpinfo ();
 systemctl enable httpd
 systemctl start httpd
 
+# prevent selinux from killing our db connection
+
+setsebool -P httpd_can_network_connect_db=1
+
 git clone https://github.com/nic-instruction/stuff.git
 
 cp stuff/app/* /var/www/html/
