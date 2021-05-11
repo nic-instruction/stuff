df991cdbea7cbed7cdb4516cb151542e2cb1d437
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-11 14:15:45 -0700

    Update tear-down-multi-zone-three-tier-two-load-ballancers.sh

--- load_ballanced_automation/tear-down-multi-zone-three-tier-two-load-ballancers.sh
+++ load_ballanced_automation/tear-down-multi-zone-three-tier-two-load-ballancers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+gcloud compute forwarding-rules delete http-cr-rule --global
+gcloud compute addresses delete lb-ipv4-1 --global
+gcloud compute target-http-proxies delete http-lb-proxy 
 
 gcloud compute health-checks delete --region us-west1 hc-http-80
 gcloud compute health-checks delete --region us-central1 hc-http-80
