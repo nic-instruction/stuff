029eeaa4d46c8308b5e78a54109f7cef77521c2f
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-11 12:27:36 -0700

    Update tear-down-multi-zone-three-tier-two-load-ballancers.sh

--- load_ballanced_automation/tear-down-multi-zone-three-tier-two-load-ballancers.sh
+++ load_ballanced_automation/tear-down-multi-zone-three-tier-two-load-ballancers.sh
@@ -3,3 +3,6 @@
 
 gcloud compute health-checks delete --region us-west1 hc-http-80
 gcloud compute health-checks delete --region us-central1 hc-http-80
+
+gcloud compute backend-services delete --region=us-west1 nic-load-balancing-backend-service-lb-w1
+gcloud compute backend-services delete --region=us-central1 nic-load-balancing-backend-service-lb-c1
